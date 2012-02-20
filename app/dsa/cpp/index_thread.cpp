f5646dc355c2c10a8c32e4178aa65f6087e15e9a
ru <htmapp@wheresjames.com>
2012-02-19 20:14:09 -0500

    + Windows compatibility

--- app/dsa/cpp/index_thread.cpp
+++ app/dsa/cpp/index_thread.cpp
@@ -6,7 +6,6 @@
 #include "htmapp.h"
 #include "cpp/file_index.h"
 
-	static volatile long g_lCancel = 0;
 	static volatile unsigned long g_lLastUpdate = 0;
 	
 	bool replace_smallest( CFileIndex::SBlockItem *p, CFileIndex::SBlockItem **lst, long sz_lst )
@@ -51,9 +50,9 @@
 		{	pb[ "name" ] = "used";
 			pb[ "colour" ] = "#800000",
 			pb[ "hi_colour" ] = "#a00000";
-			pb[ "size" ] = p->size;
+			pb[ "size" ] = ( 0 < p->size ) ? p->size : 1;
 			pb[ "path" ] = sRoot;
-			pb[ "szstr" ] = str::SizeStr< str::t_char, str::t_string >( (double)p->size, 1024., 3 ) + "B";
+			pb[ "szstr" ] = str::SizeStr< str::t_char, str::t_string >( (double)p->size, 1024., 2 ) + "B";
 			tq::set( "indexer.progress", p->size, "." );
 			return add_items( sRoot, pb[ "children" ], fi, p->child, lMaxTop, lMaxDepth, lDepth + 1 );
 		} // end if
@@ -91,7 +90,7 @@
 			add_items( disk::FilePath< str::t_char, str::t_string >( sRoot, fi.getBlob( tdir[ i ]->name ) ),
 					   r[ "children" ], fi, tdir[ i ]->child, lMaxTop, lMaxDepth, lDepth + 1 );
 		} // end for
-			
+
 		// Add top files
 		for ( long i = 0; i < lMaxTop && tfile[ i ]; i++ )		
 			add_item( sRoot, pb[ idx++ ], fi, tfile[ i ] );
@@ -102,13 +101,13 @@
 	void add_item( str::t_string sRoot, t_pb &pb, CFileIndex &fi, CFileIndex::SBlockItem *p )
 	{
 //		CFileIndex::SBlockItem *p = fi.getItem( b );
-		if ( !p )
+		if ( !p || 0 > p->size )
 			return;
-		
+
 		// Set our name
 		pb[ "name" ] = fi.getBlob( p->name );
 		pb[ "path" ] = disk::FilePath< str::t_char, str::t_string >( sRoot, fi.getBlob( p->name ) );
-		pb[ "szstr" ] = str::SizeStr< str::t_char, str::t_string >( (double)p->size, 1024., 3 ) + "B";
+		pb[ "szstr" ] = str::SizeStr< str::t_char, str::t_string >( (double)p->size, 1024., 2 ) + "B";
 		
 		if ( 0 != ( p->flags & disk::eFileAttribDirectory ) )
 //			pb[ "colour" ] = "#40a000",
@@ -117,53 +116,57 @@
 			pb[ "hi_colour" ] = "#ffa000";
 		else
 			pb[ "colour" ] = "#004080",
-			pb[ "hi_colour" ] = "#00a0FF",
+			pb[ "hi_colour" ] = "#00a0FF";
 
-			// Item size
-			pb[ "size" ] = p->size;
+			// Item size, some renderers have a problem with zero
+			pb[ "size" ] = ( 0 < p->size ) ? p->size : 1;
 	}
 	
 	long index_callback( CFileIndex *pFi, void *p )
 	{
+		// Ensure valid
 		if ( !pFi || !p )
-			return g_lCancel;
-			
-		// Recover job pointer
-		t_pb &job = *((t_pb*)p);
+			return 1;
 
 		// Update once per second
 		const unsigned long t = (const unsigned long)time( 0 );
 		if ( t == g_lLastUpdate )
-			return g_lCancel;
+			return 0;
 		g_lLastUpdate = t;
+
+		// Check for cancel
+		if ( tq::get( "indexer.job.cancel", "." ).ToLong() )
+			return 1;
+
+		// Recover job pointer
+		t_pb &job = *((t_pb*)p);
 
 		// Create a new root
 		t_pb pb;
-		t_pb &r = pb[ "children" ]; r.is_list( 1 );				
+		t_pb &r = pb[ "children" ]; r.is_list( 1 );
 
+		str::tc_int64 nFree = job[ "drive" ][ "bytes_free" ].ToInt64();
+		
 		// Add free space info
 		t_pb &pbFree = r[ "free" ];
 		pbFree[ "name" ] = "used";
 		pbFree[ "colour" ] = "#008000",
 		pbFree[ "hi_colour" ] = "#00a000";
-		pbFree[ "size" ] = job[ "drive" ][ "bytes_free" ].ToInt64();
+		pbFree[ "size" ] = ( 0 < nFree ) ? nFree : 1;
 		pbFree[ "path" ] = "[ Free Space ]";
-		pbFree[ "szstr" ] = str::SizeStr< str::t_char, str::t_string >( (double)job[ "drive" ][ "bytes_free" ].ToInt64(), 1024., 3 ) + "B";
+		pbFree[ "szstr" ] = str::SizeStr< str::t_char, str::t_string >( (double)job[ "drive" ][ "bytes_free" ].ToInt64(), 1024., 2 ) + "B";
 
 		// Add disk items
 		add_items( job[ "params" ][ "root" ].str(), r[ "used" ], *pFi, pFi->getRoot(), job[ "top" ].ToLong(), job[ "depth" ].ToLong() );
 		
 		// Set it into the thread queue
 		tq::set( "indexer.out", pb, "." );
-		
-		return g_lCancel;
+
+		return 0;
 	}
 
 	long index_thread( CThread *t, void *p )
 	{
-		// Cancel flag
-		// long *pCancel = (long*)p;
-
 		// Indexing object
 		CFileIndex fi;
 		
@@ -174,8 +177,9 @@
 			t_pb job = tq::get( "indexer.job", "." );
 			if ( job[ "run" ].ToLong() )
 			{
-				// Clear run flag
-				tq::set( "indexer.job.run", "", "." );
+				// Clear run and cancel flags
+				tq::set( "indexer.job.cancel", 0, "." );
+				tq::set( "indexer.job.run", 0, "." );
 
 				str::t_string root = job[ "params" ][ "root" ].str();
 				if ( root.length() )
@@ -185,7 +189,7 @@
 					fi.setCallback( index_callback, &job );
 					while ( 0 < fi.Index( fi.getRoot(), root.c_str(), lMin, lMin + 1, t->getStopFlag() ) ) 
 						lMin++;
-					
+
 					// Final update
 					g_lLastUpdate = 0;
 					index_callback( &fi, 0 );
